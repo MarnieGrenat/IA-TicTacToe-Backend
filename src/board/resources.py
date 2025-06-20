a2d0af6504e8c54bd85b80f13efe0c9957fa6b01
Gabriela Dellamora Paim <gabriela@dellamora.dev>
2025-06-20 02:01:33 -0300

    Minor changes

--- src/board/resources.py
+++ src/board/resources.py
@@ -2,12 +2,14 @@ from flask_restful import Resource, reqparse
 from board.board import Board
 
 def create_resources(board: Board, predict):
+    X = 1
+    O = -1
     class Reset(Resource):
         def post(self):
             board.reset()
 
             # Let MLP play first
-            board.update_board(1, predict(board.board))
+            board.update_board(X, predict(board.board))
             return {
                 'message': 'Board reset',
                 'board': board.export_board()
@@ -20,21 +22,25 @@ def create_resources(board: Board, predict):
             args = parser.parse_args()
 
             player_position = args['position']
-            player_did_play = board.update_board(-1, player_position)
+            player_did_play = board.update_board(O, player_position)
             if player_did_play:
-                # let MLP play
-                if board.check_win() == 2: # If ongoing
-                    board.update_board(1, predict(board.board))
-
-                return {
-                    'message': 'Board updated successfully',
-                    'board': board.export_board('3x3'),
-                    'resultado': board.check_win()
-                }
+                machine_play = predict(board.board)
+                if board.update_board(X, machine_play):
+                    return {
+                        'message': 'Board updated successfully',
+                        'board': board.export_board('3x3'),
+                        'resultado': board.check_win()
+                    }
+                else:
+                    return {
+                        'message': f'Failed to update board : MachinePlay={machine_play}',
+                        'board': board.export_board('3x3'),
+                        'resultado': board.check_win()
+                    }, 201
             return {
                 'message': f'Failed to update board: {player_position=}'
             }, 500
-    
+
     class Fetch(Resource):
         def get(self):
             return {
